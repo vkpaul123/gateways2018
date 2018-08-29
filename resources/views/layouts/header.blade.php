80b3b0f85aa234e2e06d2624923df8e9b488bc75
Vikramsinh Dantkale <vkpaul123@gmail.com>
2018-08-29 15:55:30 +0530

    changed logo - fixed hamburger margin

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,7 +13,7 @@
 				  	<div class="container-fluid nav-bar">
 				    <!-- Brand and toggle get grouped for better mobile display -->
 					    <div class="navbar-header">
-					      	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" style="margin-top: 10px;">
+					      	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" style="margin-top: 15px;">
 						        <span class="sr-only">Toggle navigation</span>
 						        <span class="icon-bar"></span>
 						        <span class="icon-bar"></span>
